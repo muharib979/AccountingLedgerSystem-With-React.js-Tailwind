b61e0f341a5ea8134392426b5ea44ffce3e6cace
Muhammad Amran Bhuiyan <muharib979@gmail.com>
2025-08-25 23:10:14 +0600

    added server

--- AccountingLedgerSystem/src/features/journal/JournalEntryForm.jsx
+++ AccountingLedgerSystem/src/features/journal/JournalEntryForm.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { saveJournalEntry } from "../../services/journalService";
-// import { getAccounts } from "../../services/accountService";
 
 const JournalEntryForm = () => {
   const [formData, setFormData] = useState({
